1e5200b2fbe57fbac118e494ea68568e19a72d0e
bortevik <bortevik@gmail.com>
2017-08-14 12:56:08 +0500

    Little refactor

--- src/elm/HeritageCalculation/State.elm
+++ src/elm/HeritageCalculation/State.elm
@@ -13,11 +13,8 @@ calculateHeritage model =
 calculateSonsHeritage : ( Model, ShareOfHeritage ) -> ( Model, ShareOfHeritage )
 calculateSonsHeritage ( model, restOfShare ) =
     let
-        selectedSon { selected, heritor } =
-            selected == Selected && heritor == Son
-
         sons =
-            List.filter selectedSon model.heritors
+            selectedHeritor Son model.heritors
     in
         if List.length sons > 0 then
             let
@@ -27,3 +24,12 @@ calculateSonsHeritage ( model, restOfShare ) =
                 ( { model | calculationResults = calculationResults }, ( 0, 0 ) )
         else
             ( model, restOfShare )
+
+
+selectedHeritor : Heritor -> List HeritorState -> List HeritorState
+selectedHeritor heritor heritors =
+    let
+        selectedSon state =
+            state.selected == Selected && state.heritor == heritor
+    in
+        List.filter selectedSon heritors
